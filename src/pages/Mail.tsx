b39fac0b46500c7011dad4ee581aa84ba342a9ad
Tatiana1708 <42034457+Tatiana1708@users.noreply.github.com>
2026-03-23 11:48:54 +0100

    [Blink] Restored to version: ver-ehyd05pi

--- src/pages/Mail.tsx
+++ src/pages/Mail.tsx
@@ -40,7 +40,6 @@ import {
   DropdownMenuTrigger 
 } from '@/components/ui/dropdown-menu';
 import { Badge } from '@/components/ui/badge';
-import { Card } from '@/components/ui/card';
 import { 
   Dialog, 
   DialogContent, 
